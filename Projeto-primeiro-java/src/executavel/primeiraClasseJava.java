e2a35bcf7eae082a83b12b015d9a1266bc74e93f
gabri <gabri@192.168.0.8>
2023-01-13 11:41:53 -0300

    Projeto_curso_Java

---
+++ Projeto-primeiro-java/src/executavel/primeiraClasseJava.java
@@ -0,0 +1,12 @@
+package executavel;
+
+public class primeiraClasseJava {
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro programa em Java");
+		
+	} 
+
+}
